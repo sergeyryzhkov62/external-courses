9bdc725846a0e591f4f50660f2c061f4a13f5a51
epam <epam@local.rsu.edu.ru>
2018-10-05 20:18:53 +0300

    first

---
+++ src/ex2_js-basics-part2/task01.js
@@ -0,0 +1,19 @@
+function f (x){
+    
+    var y = typeof(x);
+if (x === true){
+    console.log(undefined);   
+}
+else if (x === false){
+    console.log(undefined);  
+    
+}
+else {
+    console.log(y);
+    
+}
+}
+f('123');
+
+
+
